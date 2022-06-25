fd54171fc0dbde20985df2c55bd6671d39638034
Tabriz Movsumlu <tabriz@MacBook-Pro-Tabriz.local>
2022-06-25 21:56:51 +0300

    add axios to project

--- src/store/pizza/asyncThunks.js
+++ src/store/pizza/asyncThunks.js
@@ -1,18 +1,14 @@
+import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const fetchPizzas = createAsyncThunk(
   "pizza/fetchPizzas",
   async (_, { rejectWithValue }) => {
     try {
-      const response = await fetch(
+      const { data } = await axios.get(
         `https://62b2b163c7e53744afd01e9f.mockapi.io/pizzas`
       );
 
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-
-      const data = await response.json();
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
