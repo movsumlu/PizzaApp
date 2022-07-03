804f4f603b7f209fc55977442f0fecd429fbd6f2
Tabriz Movsumlu <tabriz@MacBook-Pro-Tabriz.local>
2022-07-03 11:17:02 +0300

    add MainLayout / refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
+import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { Header } from "components/Header";
+import { MainLayout } from "layouts/MainLayout";
 
 import { Home } from "pages/Home";
 import { Card } from "pages/Card";
 import { Checkout } from "pages/Checkout";
 import { NonFoundPage } from "pages/NonFoundPage";
 
-const App = () => {
+const App: FC = () => {
   return (
-    <div className="app">
-      <div className="wrapper">
-        <Header />
+    <Routes>
+      <Route path="/PizzaApp/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="card" element={<Card />} />
+        <Route path="checkout" element={<Checkout />} />
 
-        <Routes>
-          <Route path="PizzaApp" element={<Home />} />
-          <Route path="PizzaApp/card" element={<Card />} />
-          <Route path="PizzaApp/checkout" element={<Checkout />} />
-
-          <Route path="*" element={<NonFoundPage />} />
-        </Routes>
-      </div>
-    </div>
+        <Route path="*" element={<NonFoundPage />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { Outlet } from "react-router-dom";
+
+import { Header } from "components/Header";
+
+const MainLayout: FC = () => {
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <Header />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export { MainLayout };
